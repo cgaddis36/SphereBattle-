e2725fc1d66fff5b6e26e53108e54aab16cc5aae
Chase Gaddis <cgaddis36@gmail.com>
2023-02-24 17:38:06 -0600

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject focalPoint;
+    private Rigidbody playerRigidbody;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRigidbody.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
